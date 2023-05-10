536a1e4c76a0936930b2ecb4c7cbfef0d7859778
matiambrosi <matiambrosi@pop-os.localdomain>
2023-05-10 04:07:28 -0300

    Primer commit.

---
+++ tpintegradorspring/src/main/java/com/javaspring/tpintegradorspring/TpintegradorspringApplication.java
@@ -0,0 +1,13 @@
+package com.javaspring.tpintegradorspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpintegradorspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpintegradorspringApplication.class, args);
+	}
+
+}
---
+++ tpintegradorspring/src/test/java/com/javaspring/tpintegradorspring/TpintegradorspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaspring.tpintegradorspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpintegradorspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
