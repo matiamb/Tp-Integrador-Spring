29cecb46f4f2b9d5599cc472883c3467a719324f
matiambrosi <matiambrosi@pop-os.localdomain>
2023-05-12 05:08:04 -0300

    Creadas la tabla venta y relaciones con cliente y productos

--- tpintegradorspring/src/main/java/com/javaspring/tpintegradorspring/model/Venta.java
+++ tpintegradorspring/src/main/java/com/javaspring/tpintegradorspring/model/Venta.java
@@ -5,21 +5,34 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-//@Entity
+@Entity
 public class Venta {
-    //@Id
-    //@GeneratedValue(strategy=GenerationType.SEQUENCE)
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
     private Long codigo_venta;
     public LocalDate fecha_venta;
     public double total;
     public List<Producto> listaProductos;
     public Cliente unCliente;
+    
+    @OneToMany
+    public List<Producto> productosVta;
+    
+    @OneToOne
+    @JoinColumn(name = "id_cliente", 
+            referencedColumnName = "id_cliente")
+    public Cliente cliente;
+    
+    
 
     public Venta() {
     }
