bfc5af58aa740020b640b0bcd2f99e3dd8e88e73
matiambrosi <matiambrosi@pop-os.localdomain>
2023-05-16 03:40:29 -0300

    Relacion con venta manytoone creada pero no funciona

--- tpintegradorspring/src/main/java/com/javaspring/tpintegradorspring/model/Producto.java
+++ tpintegradorspring/src/main/java/com/javaspring/tpintegradorspring/model/Producto.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,21 +15,28 @@ import lombok.Setter;
 public class Producto {
     @Id
     @GeneratedValue(strategy=GenerationType.SEQUENCE)
-    private Long codigo_producto;
+    public Long codigo_producto;
     public String nombre;
     public String marca;
     public double costo;
     public double cantidad_disponible;
+    
+    @ManyToOne
+    @JoinColumn (name = "codigo_venta")
+    public Venta unaVenta;
 
     public Producto() {
     }
 
-    public Producto(Long codigo_producto, String nombre, String marca, double costo, double cantidad_disponible) {
+    public Producto(Long codigo_producto, String nombre, String marca, double costo, double cantidad_disponible, Venta unaVenta, Cliente unCli) {
         this.codigo_producto = codigo_producto;
         this.nombre = nombre;
         this.marca = marca;
         this.costo = costo;
         this.cantidad_disponible = cantidad_disponible;
+        this.unaVenta = unaVenta;
     }
+
+    
     
 }
