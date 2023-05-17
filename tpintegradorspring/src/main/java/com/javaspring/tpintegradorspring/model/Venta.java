a01364638ecdbd6d5292180af925c5cc57c3dc0e
matiambrosi <matiambrosi@pop-os.localdomain>
2023-05-17 02:34:48 -0300

    Arreglado el bug de que no corria, eliminados objetos de productos y venta duplicados. Agregado el mappedBy en onetomany

--- tpintegradorspring/src/main/java/com/javaspring/tpintegradorspring/model/Venta.java
+++ tpintegradorspring/src/main/java/com/javaspring/tpintegradorspring/model/Venta.java
@@ -21,28 +21,22 @@ public class Venta {
     private Long codigo_venta;
     public LocalDate fecha_venta;
     public double total;
+    @OneToMany (mappedBy = "unaVenta")
     public List<Producto> listaProductos;
-    public Cliente unCliente;
-    
-    @OneToMany
-    public List<Producto> productosVta;
-    
     @OneToOne
-    @JoinColumn(name = "id_cliente", 
+    @JoinColumn(name = "idCliente", 
             referencedColumnName = "id_cliente")
-    public Cliente cliente;
+    public Cliente unCliente;
     
     public Venta() {
     }
 
-    public Venta(Long codigo_venta, LocalDate fecha_venta, double total, List<Producto> listaProductos, Cliente unCliente, List<Producto> productosVta, Cliente cliente) {
+    public Venta(Long codigo_venta, LocalDate fecha_venta, double total, List<Producto> listaProductos, Cliente unCliente) {
         this.codigo_venta = codigo_venta;
         this.fecha_venta = fecha_venta;
         this.total = total;
         this.listaProductos = listaProductos;
         this.unCliente = unCliente;
-        this.productosVta = productosVta;
-        this.cliente = cliente;
     }
     
 }
