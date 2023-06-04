6851dd75236493ef2b7ccf6406f13fa5dc3ffd9e
Matias Ambrosi <matiambrosi@gmail.com>
2023-06-04 00:56:59 -0300

    Agregado el Jsonignore para solucionar bucle infinito en el get

--- tpintegradorspring/src/main/java/com/javaspring/tpintegradorspring/model/Cliente.java
+++ tpintegradorspring/src/main/java/com/javaspring/tpintegradorspring/model/Cliente.java
@@ -1,6 +1,7 @@
 
 package com.javaspring.tpintegradorspring.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +21,7 @@ public class Cliente {
     public String apellido;
     public String dni;
     @OneToMany (mappedBy = "cliente")
+    @JsonIgnoreProperties ("cliente")
     public List<Venta> ventas;
 
     public Cliente() {
