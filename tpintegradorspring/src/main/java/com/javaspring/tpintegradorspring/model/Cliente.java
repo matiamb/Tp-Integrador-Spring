a6bd10945888bf8821544b2f97553d75aa719e27
matiambrosi <matiambrosi@pop-os.localdomain>
2023-06-07 05:33:07 -0300

    agregado jsonignore para evitar overflow

--- tpintegradorspring/src/main/java/com/javaspring/tpintegradorspring/model/Cliente.java
+++ tpintegradorspring/src/main/java/com/javaspring/tpintegradorspring/model/Cliente.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,6 +23,7 @@ public class Cliente {
     public String dni;
     @OneToMany (mappedBy = "cliente")
     @JsonIgnoreProperties ("cliente")
+    @JsonIgnore
     public List<Venta> ventas;
 
     public Cliente() {
