6023cdc239282932199793a379bac9a646611a06
matiambrosi <matiambrosi@pop-os.localdomain>
2023-06-07 05:28:03 -0300

    eliminadas dependencias innecesarias

--- tpintegradorspring/src/main/java/com/javaspring/tpintegradorspring/model/Producto.java
+++ tpintegradorspring/src/main/java/com/javaspring/tpintegradorspring/model/Producto.java
@@ -1,12 +1,10 @@
 
 package com.javaspring.tpintegradorspring.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import java.util.List;
 import lombok.Getter;
